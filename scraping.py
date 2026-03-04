40ef15751c051282fc527d5f1afe85b1a1aca021
devLuizPozza <luizpozza.2019@gmail.com>
2026-03-04 07:24:00 -0300

    adjust scraping

--- scraping.py
+++ scraping.py
@@ -2,17 +2,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Configurações
-
-
 produto = input('Digite um produto: ')
 produto = produto.replace(' ', '-')
 
 url = f'https://lista.mercadolivre.com.br/{produto}'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36"}
-
-
-
 
 start = 1
 
