dc68ec6e67a9d5878529c886d62db5beb2841621
Luiz <luizpozza.2019@hotmail.com>
2026-03-05 14:37:00 -0300

    adjust max trheads and max itens

--- pro_scrapping.py
+++ pro_scrapping.py
@@ -10,10 +10,10 @@ produto = input("Digite o produto: ").strip().replace(" ", "-")
 BASE_URL = f"https://lista.mercadolivre.com.br/{produto}_Desde_"
 
 USER_AGENTS = [
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
-"Mozilla/5.0 (X11; Linux x86_64)",
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)",
+    "Mozilla/5.0 (X11; Linux x86_64)",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/120 Safari/537.36"
 ]
 
 session = requests.Session()
@@ -44,8 +44,11 @@ def coletar_pagina(start):
 
             items = soup.select("li.ui-search-layout__item")
 
-            if not items:
-                return []
+            # detector de bloqueio ou fim das páginas
+            if len(items) == 0:
+                print("Possível bloqueio ou fim das páginas. Aguardando 2 minutos...")
+                time.sleep(120)
+                return coletar_pagina(start)
 
             pagina_produtos = []
 
@@ -77,12 +80,12 @@ def coletar_pagina(start):
                 except:
                     pass
 
-            time.sleep(random.uniform(3,7))
+            time.sleep(random.uniform(3, 7))
 
             return pagina_produtos
 
         except:
-            time.sleep(random.uniform(3,7))
+            time.sleep(random.uniform(3, 7))
 
     return []
 
@@ -90,10 +93,10 @@ def coletar_pagina(start):
 print("Iniciando scraping...")
 
 # offsets de paginação (48 produtos por página)
-offsets = list(range(1, 2000, 48))
+offsets = list(range(1, 700, 48))
 
 
-with ThreadPoolExecutor(max_workers=10) as executor:
+with ThreadPoolExecutor(max_workers=4) as executor:
 
     futures = [executor.submit(coletar_pagina, offset) for offset in offsets]
 
@@ -104,11 +107,11 @@ with ThreadPoolExecutor(max_workers=10) as executor:
         if resultado:
             produtos.extend(resultado)
 
+
 print("Total coletado:", len(produtos))
 
 
 with open(f"{produto}_mercadolivre.json", "w", encoding="utf-8") as f:
-
     json.dump(produtos, f, indent=4, ensure_ascii=False)
 
 
